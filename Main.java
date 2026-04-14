da98de0500b50bc7874e12f0b9955599a06cb326
Job Daniel Correa <jobcorre0912@gmail.com>
2026-04-14 13:11:38 -0500

    Actualizando Main con clase Estudiante

--- Main.java
+++ Main.java
@@ -1,3 +1,10 @@
 public class Main{
-    
+
+    public static void main(String[] args) {
+        Estudiante study1 = new Estudiante("Job", 15, -1); //Ivalido
+        study1.mostrarInfo();
+        study1.setEdad(22);
+        study1.setNotaPromedio(5);
+        study1.mostrarInfo();
+    }
 }
