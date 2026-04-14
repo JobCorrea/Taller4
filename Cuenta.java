b70eb2bcef0bd3fc9fee57b9bc3caf2faa38904e
Job Daniel Correa <jobcorre0912@gmail.com>
2026-04-14 14:17:33 -0500

    Ejercicio punto 2 - get y set

---
+++ Cuenta.java
@@ -0,0 +1,11 @@
+public class Cuenta {
+    private double saldo;
+
+    public Cuenta(double saldo) {
+        this.saldo = saldo;
+    }
+
+    //  Sin getter ni setter
+    // No hay forma de leer ni modificar el saldo
+}
+
