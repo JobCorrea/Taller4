377327f06447027e0fe1590b3190d3be87610b82
Job Daniel Correa <jobcorre0912@gmail.com>
2026-04-14 13:56:57 -0500

    Actualizacion de main y creacion de objeto coche

--- Main.java
+++ Main.java
@@ -6,5 +6,20 @@ public class Main{
         study1.setEdad(22);
         study1.setNotaPromedio(5);
         study1.mostrarInfo();
+
+        //Coche
+        Coche auto1 = new Coche("aaa", null, 0);
+        auto1.mostrarInfo();
+        auto1.setMarca("Auudi");
+        auto1.setModelo("i10");
+        auto1.setVelocidadMaxima(100);
+        auto1.mostrarInfo();
+        auto1.acelerar(10);
+        auto1.mostrarInfo();
+        // Intento de acceso directo a atributos privados
+        // System.out.println(auto.marca);          
+        // System.out.println(auto.modelo);         
+        // System.out.println(auto.velocidadMaxima);
+        //variable marca is never read
     }
 }
